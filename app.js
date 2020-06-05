2e3e43397308dcd84f7750021cc51439c5492741
kickstv <kicksmedia1@gmail.com>
2020-06-05 23:28:06 +0100

    User is now able to load project templates

--- app.js
+++ app.js
@@ -36,6 +36,25 @@ app.get('/projects/newproject', function (req, res) {
     res.render('newproject', {pagedata:pagedata, user:user});
 });
 
+for (let project of defaultProjects.projects) {
+    app.get(`/projects/loadproject/${project.projectName}`, function (req, res) {
+        var user;
+        var pagedata = {
+            'title': project.projectName,
+            'navExpand': 'show',
+            'project': project.json
+        };
+        res.render('loadedProject', {pagedata:pagedata, user:user});
+    });
+}
+
+for (let project of defaultProjects.projects) {
+    app.get(`/projects/loadproject/${project.projectName}/get`, function (req, res) {
+        res.json(JSON.stringify(project));
+    });
+}
+
+
 var server = app.listen(5000, () => {
     console.log("The server is now running on port 5000!");
 });
@@ -45,21 +64,19 @@ let io = socket.listen(server);
 io.sockets.on('connection',
     function (socket) {
         console.log("We have a new client: " + socket.id);
-    
+
+        clientId=socket.handshake.query.clientId;
+        socket.broadcast.emit('connection', null);
+
+        socket.on('sentCanvasData',
+            function(data) {
+            // Send it to all other clients
+            socket.broadcast.emit('receivedCanvasData', data);
+            }
+        );
         socket.on('disconnect', function() {
             console.log("Client has disconnected");
         });
-
-
-        socket.on('mouse',
-            function(data) {
-            // Data comes in as whatever was sent, including objects
-            // console.log("Received: 'mouse' " + data.x + " " + data.y);      
-            // Send it to all other clients
-            socket.broadcast.emit('mouse', data);
-            }
-        );  
-
         socket.on('move',
             function(data) {
             // Data comes in as whatever was sent, including objects
@@ -68,18 +85,22 @@ io.sockets.on('connection',
             socket.broadcast.emit('move', data);
             }
         );
-
         socket.on('createComponent',
             function(data) {
             // Send it to all other clients
             socket.broadcast.emit('createComponent', data);
             }
         );
-
         socket.on('componentMove',
             function(data) {
             // Send it to all other clients
             socket.broadcast.emit('componentMove', data);
+            }
+        );
+        socket.on('componentChange',
+            function(data) {
+            // Send it to all other clients
+            socket.broadcast.emit('componentChange', data);
             }
         );
     }
