6839e756865d4b9b2d1c67b233023a5131924bc7
kickstv <kicksmedia1@gmail.com>
2020-06-05 23:31:16 +0100

    When a new client is connection, client will load project.

--- assets/js/controller/ioController.js
+++ assets/js/controller/ioController.js
@@ -1,4 +1,5 @@
 import componentController from './componentController.js';
+import saveLoadController from './saveLoadController.js';
 import allComponents from '../collections/allComponents.js';
 import Graph from '../models/graph.js';
 
@@ -13,25 +14,36 @@ const ioController = (function() {
 
 
         function initIO() {
-            socket = io.connect('http://localhost:5000');
+            socket = io.connect(`http://localhost:5000/?clientId=${clientId}`);
 
-            // We make a named event called 'mouse' and write an
+            // We make a named event called 'createComponent' and write an
             // anonymous callback function
-            socket.on('mouse',
-                function(data) {
-                    console.log("Got: " + data.x + " " + data.y);
-                    push(); // Start a new drawing state
-                    // Draw a blue circle
-                    fill(0,0,255);
-                    noStroke();
-                    ellipse(data.x,data.y,80,80);
-                    pop(); // Restore original state
+            socket.on('connection', 
+                async function(data) {
+                    console.log("new client connected");
+
+                    let json = await saveLoadController.getInstance().saveEventToJSON();
+                    console.log(json);
+                    if (json != null) {
+                        sendData('sentCanvasData', json);
+                    }
                 }
             );
-    
+
+            socket.on('receivedCanvasData', 
+                async function(data) {
+                    console.log("Got canvas data", data);
+
+                    // let json = await saveLoadController.getInstance().saveEventToJSON();
+                    // console.log(json);
+                    saveLoadController.getInstance().loadProject(data);
+                    // sendData('sentCanvasData', json);
+                }
+            );
+
             socket.on('createComponent',
                 async function(data) {
-                    console.log("Got:", data);
+                    // console.log("Got:", data);
 
                     let defaultComponent = await componentController.getInstance().createNewComponent(data.name);
 
@@ -44,17 +56,15 @@ const ioController = (function() {
                     Graph.getInstance().addNode(newcomp.id);
                 }
             );
-
             socket.on('componentMove',
                 async function(data) {
-                    console.log("Got:", data);
-
+                    // console.log("Got:", data);
                     var foundComponent = allComponents.getInstance().getAll().find(c => c.id == data.id);
-                    console.log(foundComponent);
-                    foundComponent.move(data.x, data.y);
+                    if (foundComponent) {
+                        foundComponent.move(data.x, data.y);
+                    }
                 }
             );
-
             socket.on('move',
                 function(data) {
                     // console.log("Got: " + data.x + " " + data.y);
@@ -66,13 +76,22 @@ const ioController = (function() {
                     pop(); // Restore original state
                 }
             );
+            socket.on('componentChange',
+                function(data) {
+                    var foundComponent = allComponents.getInstance().getAll().find(c => c.id == data.id);
 
-            console.log(socket);
+                    var newcomp = Object.assign(foundComponent, data);
 
+                    newcomp.image.width = data.width;
+                    newcomp.image.height = data.height;
+
+                    allComponents.getInstance().getAll().map(item => item.id === data.id ? newcomp : item);
+                }
+            );
         }
         function sendData(event , data) {
             // Send that object to the socket
-            console.log("Sending Data");
+            // console.log("Sending Data");
             socket.emit(event, data);
         }
 
