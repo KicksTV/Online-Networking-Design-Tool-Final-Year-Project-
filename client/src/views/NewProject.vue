6cd371cf2e94d4e481b560d7fadcc4e346f602e5
kickstv <kicksmedia1@gmail.com>
2022-04-06 10:13:23 +0100

    Hiding p5 vue

--- client/src/views/NewProject.vue
+++ client/src/views/NewProject.vue
@@ -2,7 +2,6 @@
   <div>
     <ComponentBar></ComponentBar>
     <Canvas></Canvas>
-    <p5></p5>
     <BottomPanel></BottomPanel>
   </div>
 </template>
@@ -12,7 +11,7 @@
   import ComponentBar from '@/components/ComponentBar.vue'
   import Canvas from '@/components/Canvas.vue'
   import BottomPanel from '@/components/BottomPanel.vue'
-  import p5 from '@/components/p5.vue'
+  // import p5 from '@/components/p5.vue'
 
   export default {
     name: 'NewProject',
@@ -20,7 +19,7 @@
       ComponentBar,
       Canvas,
       BottomPanel,
-      p5
+      // p5
     },
     mounted() {
         var self = this;
